b63ef20e40b2e2b8bb1a97d0bff3271e64a46e1b
Nan <akks.crx@gmail.com>
2018-12-08 21:13:17 +0900

    add parameters

---
+++ config/Parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef PARAMETERS_HPP
+#define PARAMETERS_HPP
+
+const double Magnet_Field = 0.0;
+const double J_NN = 1.0;
+const double J_NNN = 0.0;
+const bool USE_NNN = false;
+const int default_n = 100;
+const int default_m = 100;
+
+#endif
--- lattice/lattice.hpp
+++ lattice/lattice.hpp
@@ -1,23 +1,33 @@
 #ifndef LATTICE_HPP
 #define LATTICE_HPP
 
+#include <config/Parameters.hpp>
 class ISING_LATTICE
 {
 private:
 	bool **a;
-	int n;
+	int n, m;
 
 public:
-	ISING_LATTICE(int N): n(N)
+	ISING_LATTICE(int N, int M): n(N), m(M)
 	{
 		a = new bool*[N];
 		for (int i = 0; i < N; ++i)
 		{
-			a[i] = new bool[N];
-			for (int j = 0; j < N; ++j)
+			a[i] = new bool[M];
+			for (int j = 0; j < M; ++j)
 				a[i][j] = 0;
 		}
 	}
-}
+	bool getS(int x, int y)
+	{
+		return a[(x + n) % n][(y + m) % m];
+	}
+	void setS(int x, int y, bool value)
+	{
+		a[(x + n) % n][(y + m) % m] = value;
+	}
+};
+
 #endif
 
